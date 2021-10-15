ce97a392982ce04ed8c758fc737b885b55bba8d2
Zahid Kizmaz <tech@zahid.rocks>
2021-10-15 17:58:55 +0200

    Make first request to nasa api
    * Install aiohttp
    * Create apod view

--- api/apod/views.py
+++ api/apod/views.py
@@ -1,8 +1,16 @@
+from urllib.parse import urljoin
+
+import aiohttp
 from fastapi import APIRouter
+
+from settings.config import settings
 
 router = APIRouter()
 
 
 @router.get("/")
-async def hello_world():
-    return {"message": "Hello, world!"}
+async def get_apod():
+    apod_url = urljoin(settings.NASA_API_URL, settings.APOD_URL_PATH)
+    async with aiohttp.ClientSession() as session:
+        async with session.get(apod_url, params=settings.NASA_API_KEY) as response:
+            return await response.json()

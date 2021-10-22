ae3dc2611f2f412556bcac13097c46d0f2a8c877
Zahid Kizmaz <tech@zahid.rocks>
2021-10-22 22:43:35 +0200

    Create apod view and template

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import uvicorn
+from starlette.templating import Jinja2Templates
 
 from api import create_app
 from settings.config import settings
 
 app = create_app()
+templates = Jinja2Templates(directory="templates")
 
 
 def main() -> None:

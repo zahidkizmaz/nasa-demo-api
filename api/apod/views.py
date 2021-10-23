c8bfcad59a03ecd0f0e1824b72012fc93ca725bb
Zahid Kizmaz <tech@zahid.rocks>
2021-10-23 21:11:30 +0200

    Add tests and create NasaApiService

--- api/apod/views.py
+++ api/apod/views.py
@@ -1,17 +1,11 @@
-from urllib.parse import urljoin
-
-import aiohttp
 from fastapi import APIRouter
 
-from settings.config import settings
+from api.services import get_nasa_api_service
 
 router = APIRouter()
-
-APOD_URL = urljoin(settings.NASA_API_URL, settings.APOD_URL_PATH)
 
 
 @router.get("/")
 async def get_apod():
-    async with aiohttp.ClientSession() as session:
-        async with session.get(APOD_URL, params=settings.NASA_API_KEY) as response:
-            return await response.json()
+    nasa_api_service = get_nasa_api_service()
+    return await nasa_api_service.get_apod()
